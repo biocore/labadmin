746e9050f2b9df382c68ff872a3433dee3b6bf5d
Joshua Shorenstein <squirrelo@gmail.com>
2015-07-27 17:17:32 -0700

    add barcodes through interface

--- knimin/handlers/ag_new_barcode.py
+++ knimin/handlers/ag_new_barcode.py
@@ -3,6 +3,7 @@ from tornado.web import authenticated, HTTPError
 from knimin.handlers.base import BaseHandler
 
 from amgut.connections import ag_data
+from knimin import db
 
 
 class AGNewBarcodeHandler(BaseHandler):
@@ -10,26 +11,28 @@ class AGNewBarcodeHandler(BaseHandler):
     def get(self):
         project_names = ag_data.getProjectNames()
         self.render("ag_new_barcode.html", currentuser=self.current_user,
-                    projects=project_names, barcodes=[])
+                    projects=project_names, barcodes=[], msg="")
 
     @authenticated
     def post(self):
         barcodes = []
         action = self.get_argument('action')
         if action == 'create':
-            projects = self.get_arguments('projects', [])
+            projects = self.get_arguments('projects')
             new_project = self.get_argument('newproject').strip()
-            num_barcodes = self.get_argument('numbarcodes')
+            num_barcodes = int(self.get_argument('numbarcodes'))
             if new_project:
-                ag_data.create_project(new_project)
+                db.create_project(new_project)
                 projects.append(new_project)
-
-            
-
+            new_barcodes = db.create_barcodes(num_barcodes, projects)
+            barcodes_info = db.get_barcode_details(new_barcodes)
+            msg = "%d Barcodes created!" % num_barcodes
         elif action == 'view':
-
+            barcodes_info = {}
         else:
-            raise HTTPError(400, 'Unexpected action: %s' % action)
+            raise RuntimeError("Unknown action for AGNewBarcdeHandler: %s" %
+                               action)
+
         project_names = ag_data.getProjectNames()
         self.render("ag_new_barcode.html", currentuser=self.current_user,
-                    projects=project_names, barcodes=barcodes)
+                    projects=project_names, barcodes=barcodes_info, msg=msg)
