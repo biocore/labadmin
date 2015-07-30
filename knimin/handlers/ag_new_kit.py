7e9ebbdcaa5ee978498b0ce7761d9a8ba86e603d
Joshua Shorenstein <squirrelo@gmail.com>
2015-07-29 23:54:44 -0700

    get printouts

--- knimin/handlers/ag_new_kit.py
+++ knimin/handlers/ag_new_kit.py
@@ -1,9 +1,20 @@
 #!/usr/bin/env python
+from json import dumps, loads
 from tornado.web import authenticated
+from tornado.escape import urlencode
 from knimin.handlers.base import BaseHandler
 from knimin import db
+from knimin.lib.mem_zip import InMemoryZip
+from knimin.lib.util import get_printout_data
 from amgut.connections import ag_data
 
+
+class AGNewKitDLHandler(BaseHandler):
+    @authenticated
+    def get(self):
+        kitinfo = loads(self.get_argument('kitinfo'))
+        kit_zip = InMemoryZip()
+        kit_zip.append('kit_printouts.txt', get_printout_data(kitinfo))
 
 class AGNewKitHandler(BaseHandler):
     @authenticated
@@ -19,7 +30,7 @@ class AGNewKitHandler(BaseHandler):
         num_swabs = map(int, self.get_arguments("swabs"))
         num_kits = map(int, self.get_arguments("kits"))
         try:
-            db.create_ag_kits(zip(num_swabs, num_kits), tag, projects)
+            kits = db.create_ag_kits(zip(num_swabs, num_kits), tag, projects)
         except Exception as e:
             msg = "ERROR: %s" % str(e)
         else:
@@ -27,4 +38,5 @@ class AGNewKitHandler(BaseHandler):
 
         project_names = ag_data.getProjectNames()
         self.render("ag_new_kit.html", projects=project_names,
-                    currentuser=self.current_user, msg=msg)
+                    currentuser=self.current_user, msg=msg,
+                    kitinfo=urlencode(dumps(kits)))
