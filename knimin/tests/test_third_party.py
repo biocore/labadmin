b2e88bc0b88f52b62c0a0c9f556ad5a255900592
Joshua Shorenstein <squirrelo@gmail.com>
2015-10-30 15:56:45 -0700

    all tests working

--- knimin/tests/test_third_party.py
+++ knimin/tests/test_third_party.py
@@ -1,5 +1,7 @@
 from unittest import main
 from os.path import dirname, realpath, join
+from string import ascii_letters
+from random import choice
 
 from knimin import db
 from knimin.tests.tornado_test_base import TestHandlerBase
@@ -60,6 +62,62 @@ class TestThirdPartyData(TestHandlerBase):
                       '<li>Not a valid choice', response.body)
         db._clear_table('external_survey_answers', 'ag')
 
+
+class TestNewThirdParty(TestHandlerBase):
+    # Make sure vioscreen survey exists in DB
+    try:
+        db.add_external_survey('Vioscsreen', 'FFQ', 'http://vioscreen.com')
+    except ValueError:
+        pass
+
+    def test_get_not_authed(self):
+        response = self.get('/ag_third_party/add/')
+        self.assertEqual(response.code, 200)
+        self.assertTrue(response.effective_url.endswith(
+            '?next=%2Fag_third_party%2Fadd%2F'))
+
+    def test_get(self):
+        self.mock_login()
+        response = self.get('/ag_third_party/add/')
+        self.assertEqual(response.code, 200)
+        self.assertIn('<label for="description">Description</label>',
+                      response.body)
+
+    def test_post_not_authed(self):
+        name = ''.join([choice(ascii_letters) for x in range(15)])
+        response = self.post('/ag_third_party/add/',
+                             data={'name': name, 'description': 'TEST',
+                                   'url': 'test.fake'})
+        self.assertEqual(response.code, 403)
+
+    def test_post_data(self):
+        self.mock_login()
+        name = ''.join([choice(ascii_letters) for x in range(15)])
+        response = self.post('/ag_third_party/add/',
+                             data={'name': name, 'description': 'TEST',
+                                   'url': 'test.fake'})
+        self.assertEqual(response.code, 200)
+        self.assertIn("Added '%s' successfully" % name, response.body)
+
+    def test_post_missing_data(self):
+        self.mock_login()
+        name = ''.join([choice(ascii_letters) for x in range(15)])
+        data = {'name': name, 'description': 'TEST', 'url': ''}
+
+        response = self.post('/ag_third_party/add/', data)
+        self.assertEqual(response.code, 200)
+        self.assertIn('Survey URL</label>\n\n<ul class="errors"><li>'
+                      'Required field', response.body)
+        db._clear_table('external_survey_answers', 'ag')
+
+    def test_post_existing_survey(self):
+        self.mock_login()
+        data = {'name': 'Vioscreen', 'description': 'TEST', 'url': 'test.fake'}
+
+        response = self.post('/ag_third_party/add/', data)
+        self.assertEqual(response.code, 200)
+        self.assertIn("Survey 'Vioscreen' already exists", response.body)
+        db._clear_table('external_survey_answers', 'ag')
 
 if __name__ == "__main__":
     main()
