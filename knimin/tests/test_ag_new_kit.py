eb0500b50951bc94c71cc1f26558215b2c580e8d
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-09-19 16:25:58 -0700

    completed the tests

--- knimin/tests/test_ag_new_kit.py
+++ knimin/tests/test_ag_new_kit.py
@@ -1,6 +1,7 @@
 from unittest import main
 
 from tornado.escape import url_escape
+from json import loads, dumps
 
 from knimin.tests.tornado_test_base import TestHandlerBase
 from knimin import db
@@ -12,7 +13,24 @@ class TestAGNewKitDLHandler(TestHandlerBase):
         self.assertEqual(response.code, 405)  # Method Not Allowed
 
     def test_post(self):
-        print("AGNewKitDLHandler -> post: empty")
+        self.mock_login_admin()
+        kits = [1, 2]
+        swabs = [2, 2]
+        tag = 'abc'
+
+        # fake kitinfo data structure
+        kitinfo = [["xxx_pggwy", "96812490", "23577",
+                    ["000033914", "000033915"]],
+                   ["xxx_drcrv", "33422033", "56486",
+                    ["000033916", "000033917"]]]
+        response = self.post('/ag_new_kit/download/',
+                             {'kitinfo': dumps(kitinfo),
+                              'fields': ('kit_id,password,verification_code,'
+                                         'barcodes')})
+        self.assertEqual(response.code, 200)
+        # is it necessary to unpack the zipfile and check its content?
+        self.assertEqual(response.headers['Content-Disposition'],
+                         'attachment; filename=kitinfo.zip')
 
 
 class TestAGNewKitHandler(TestHandlerBase):
@@ -25,11 +43,87 @@ class TestAGNewKitHandler(TestHandlerBase):
                          (port, url_escape('/ag_new_kit/')))
 
     def test_get(self):
-        print("AGNewKitHandler -> get: empty")
+        self.mock_login_admin()
+        response = self.get('/ag_new_kit/')
+        self.assertEqual(response.code, 200)
+        for project in db.getProjectNames():
+            self.assertIn("<option value='%s'>%s</option>" %
+                          (project, project), response.body)
+        self.assertIn("%i</span> unassigned barcodes" %
+                      len(db.get_unassigned_barcodes()), response.body)
 
     def test_post(self):
-        print("AGNewKitHandler -> post: empty")
+        self.mock_login_admin()
+        kits = [1, 2]
+        swabs = [2, 2]
+        tag = 'abc'
 
+        # check for correct results
+        response = self.post('/ag_new_kit/',
+                             {'tag': tag,
+                              'projects': ['PROJECT2', 'PROJECT5'],
+                              'swabs': swabs,
+                              'kits': kits,
+                              })
+        kitinfo = loads(response.body)
+        self.assertEqual(len(kitinfo['kitinfo']), sum(kits))
+        for k in kitinfo['kitinfo']:
+            self.assertIn(tag, k[0])
+        self.assertEqual(len(kitinfo['kitinfo'][0][-1]), swabs[0])
+        self.assertEqual(len(kitinfo['kitinfo'][1][-1]), swabs[1])
+        self.assertEqual(kitinfo['fields'],
+                         "kit_id,password,verification_code,barcodes")
+
+        # missing argument
+        response = self.post('/ag_new_kit/',
+                             {'projects': ['PROJECT2', 'PROJECT5'],
+                              'swabs': swabs,
+                              'kits': kits,
+                              })
+        self.assertEqual(response.code, 400)
+
+        # too long tag
+        response = self.post('/ag_new_kit/',
+                             {'tag': 'toolongtag',
+                              'projects': ['PROJECT2', 'PROJECT5'],
+                              'swabs': swabs,
+                              'kits': kits,
+                              })
+        self.assertEqual(response.code, 500)
+        self.assertIn("Tag must be 4 or less characters", response.body)
+
+        # check project existens
+        response = self.post('/ag_new_kit/',
+                             {'tag': 'abc',
+                              'projects': ['doesNotExist', 'PROJECT5'],
+                              'swabs': swabs,
+                              'kits': kits,
+                              })
+        self.assertEqual(response.code, 500)
+        self.assertIn("Project(s) given don\'t exist in database:",
+                      response.body)
+
+        # check for empty swabs list
+        response = self.post('/ag_new_kit/',
+                             {'tag': tag,
+                              'projects': ['PROJECT2', 'PROJECT5'],
+                              'swabs': [],
+                              'kits': kits,
+                              })
+        self.assertEqual(response.code, 500)
+        self.assertIn("SET assigned_on = NOW() WHERE barcode IN ()",
+                      response.body)
+
+        # no kits given
+        response = self.post('/ag_new_kit/',
+                             {'tag': tag,
+                              'projects': ['PROJECT2', 'PROJECT5'],
+                              'swabs': swabs,
+                              'kits': [],
+                              })
+        self.assertEqual(response.code, 500)
+        self.assertIn("SET assigned_on = NOW() WHERE barcode IN ()",
+                      response.body)
 
 if __name__ == "__main__":
     main()
