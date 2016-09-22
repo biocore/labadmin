fd934e5bc8629831d27292919ba81a150af7faf5
Stefan Janssen <stefan.m.janssen@gmail.com>
2016-09-22 11:29:20 -0700

    fixed comments

--- knimin/tests/test_ag_pulldown.py
+++ knimin/tests/test_ag_pulldown.py
@@ -8,70 +8,70 @@ from knimin.tests.tornado_test_base import TestHandlerBase
 from knimin import db
 
 
-# class testUpdateEBIStatusHandler(TestHandlerBase):
-#     def test_get_not_authed(self):
-#         os.environ["ASYNC_TEST_TIMEOUT"] = "30"
-#         response = self.get('/update_ebi/')
-#         self.assertEqual(response.code, 200)
-#         port = self.get_http_port()
-#         self.assertEqual(response.effective_url,
-#                          'http://localhost:%d/login/?next=%s' %
-#                          (port, url_escape('/update_ebi/')))
-#
-#     def test_get(self):
-#         self.mock_login_admin()
-#         os.environ["ASYNC_TEST_TIMEOUT"] = "30"
-#
-#         # test successful query
-#         response = self.get('/update_ebi/')
-#         self.assertEqual(response.code, 200)
-#         self.assertIn('Successfully updated barcodes in database',
-#                       response.body)
-#
-#         # TODO: I cannot see how I can raise an Exception, since there are no
-#         # input arguments necessary for the get() method
+class testUpdateEBIStatusHandler(TestHandlerBase):
+    def test_get_not_authed(self):
+        os.environ["ASYNC_TEST_TIMEOUT"] = "30"
+        response = self.get('/update_ebi/')
+        self.assertEqual(response.code, 200)
+        port = self.get_http_port()
+        self.assertEqual(response.effective_url,
+                         'http://localhost:%d/login/?next=%s' %
+                         (port, url_escape('/update_ebi/')))
+
+    def test_get(self):
+        self.mock_login_admin()
+        os.environ["ASYNC_TEST_TIMEOUT"] = "30"
+
+        # test successful query
+        response = self.get('/update_ebi/')
+        self.assertEqual(response.code, 200)
+        self.assertIn('Successfully updated barcodes in database',
+                      response.body)
+
+        # TODO: I cannot see how I can raise an Exception, since there are no
+        # input arguments necessary for the get() method
 
 
-# class testAGPulldownHandler(TestHandlerBase):
-#     file_empty = join(dirname(realpath(__file__)), 'data', 'barcodes.txt')
-#
-#     def test_get_not_authed(self):
-#         # os.environ["ASYNC_TEST_TIMEOUT"] = "30"
-#         response = self.get('/ag_pulldown/')
-#         self.assertEqual(response.code, 200)
-#         port = self.get_http_port()
-#         self.assertEqual(response.effective_url,
-#                          'http://localhost:%d/login/?next=%s' %
-#                          (port, url_escape('/ag_pulldown/')))
-#
-#     def test_get(self):
-#         self.mock_login_admin()
-#         response = self.get('/ag_pulldown/')
-#         self.assertEqual(response.code, 200)
-#         for survey in db.list_external_surveys():
-#             self.assertIn("<option value='%s'>%s</option>" % (survey, survey),
-#                           response.body)
-#         self.assertNotIn('<input type="submit" disabled>', response.body)
-#
-#     def test_post(self):
-#         self.mock_login_admin()
-#
-#         # check that warnings pop up, if no barcode file is provided
-#         data = {}
-#         files = {'somethingelse': self.file_empty}
-#         response = self.multipart_post('/ag_pulldown/', data, files)
-#         self.assertEqual(response.code, 200)
-#         self.assertIn("<div style='color:red;'>%s</div>" % ("No barcode file "
-#                       "given, thus nothing could be pulled down."),
-#                       response.body)
-#
-#         data = {}
-#         files = {'barcodes': self.file_empty}
-#         response = self.multipart_post('/ag_pulldown/', data, files)
-#         self.assertEqual(response.code, 200)
-#         self.assertIn(('<h3 style="color:red">Pulldown Processing, please wait'
-#                        ' for file download. It may take a while with many '
-#                        'barcodes.</h3>'), response.body)
+class testAGPulldownHandler(TestHandlerBase):
+    file_empty = join(dirname(realpath(__file__)), 'data', 'barcodes.txt')
+
+    def test_get_not_authed(self):
+        # os.environ["ASYNC_TEST_TIMEOUT"] = "30"
+        response = self.get('/ag_pulldown/')
+        self.assertEqual(response.code, 200)
+        port = self.get_http_port()
+        self.assertEqual(response.effective_url,
+                         'http://localhost:%d/login/?next=%s' %
+                         (port, url_escape('/ag_pulldown/')))
+
+    def test_get(self):
+        self.mock_login_admin()
+        response = self.get('/ag_pulldown/')
+        self.assertEqual(response.code, 200)
+        for survey in db.list_external_surveys():
+            self.assertIn("<option value='%s'>%s</option>" % (survey, survey),
+                          response.body)
+        self.assertNotIn('<input type="submit" disabled>', response.body)
+
+    def test_post(self):
+        self.mock_login_admin()
+
+        # check that warnings pop up, if no barcode file is provided
+        data = {}
+        files = {'somethingelse': self.file_empty}
+        response = self.multipart_post('/ag_pulldown/', data, files)
+        self.assertEqual(response.code, 200)
+        self.assertIn("<div style='color:red;'>%s</div>" % ("No barcode file "
+                      "given, thus nothing could be pulled down."),
+                      response.body)
+
+        data = {}
+        files = {'barcodes': self.file_empty}
+        response = self.multipart_post('/ag_pulldown/', data, files)
+        self.assertEqual(response.code, 200)
+        self.assertIn(('<h3 style="color:red">Pulldown Processing, please wait'
+                       ' for file download. It may take a while with many '
+                       'barcodes.</h3>'), response.body)
 
 
 class testAGPulldownDLHandler(TestHandlerBase):
@@ -105,7 +105,7 @@ class testAGPulldownDLHandler(TestHandlerBase):
                               'external': db.list_external_surveys()[:1]})
         self.assertEqual(response.headers['Content-Disposition'],
                          'attachment; filename=metadata.zip')
-        self.assertIn('failures.txt', response.body)
+        self.assertIn('failures.txt', responsadde.body)
         self.assertIn('survey_1_md.txt', response.body)
 
 if __name__ == "__main__":
