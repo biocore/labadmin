91912c287497e89b97f0be4823b90127672cdd20
Jose Navas <josenavasmolina@gmail.com>
2017-03-23 15:58:14 -0700

    Fixing knimin/tests/test_projects_summary.py

--- knimin/handlers/projects_summary.py
+++ knimin/handlers/projects_summary.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 from tornado.web import authenticated
-from tornado.escape import xhtml_escape
 from knimin.handlers.base import BaseHandler
 from knimin.handlers.access_decorators import set_access
 from knimin import db
@@ -12,6 +11,6 @@ class ProjectsSummaryHandler(BaseHandler):
     def get(self):
         projects = db.getProjectNames()
         # escape unicode project names for xhtml rendering
-        info = [(xhtml_escape(p), len(db.get_barcodes_for_projects([p])))
+        info = [(p, len(db.get_barcodes_for_projects([p])))
                 for p in projects]
         self.render('projects_summary.html', proj_counts=info)
