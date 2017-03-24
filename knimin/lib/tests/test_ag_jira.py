a8f5b1f260e64e92131a131850037728b2d91c97
Antonio Gonzalez <antgonza@gmail.com>
2017-03-24 16:06:08 -0600

    jira_connect raises error

--- knimin/lib/tests/test_ag_jira.py
+++ knimin/lib/tests/test_ag_jira.py
@@ -4,6 +4,10 @@ from knimin.lib.ag_jira import jira_connect, get_projects
 from jira.resources import Project
 
 class TestJira(TestCase):
+    def test_jira_connect_error(self):
+        with self.assertRaises(ValueError):
+            jira_connect(user='my_user')
+
     def test_get_projects(self):
         projects = get_projects()
         for p in projects:
