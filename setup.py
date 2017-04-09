8f762b13c940033892f040e89916961ec6eeb1ae
Jose Navas <josenavasmolina@gmail.com>
2017-04-09 00:20:41 -0700

    reverting setup

--- setup.py
+++ setup.py
@@ -29,6 +29,5 @@ setup(name='Knight lab UI',
                                "requests-mock"]},
       install_requires=['psycopg2 < 2.7', 'tornado==4.4.2', 'WTForms==2.0.1',
                         'future', 'bcrypt', 'pillow', 'python-dateutil',
-                        'requests', 'mock', 'pandas', 'six', 'jira',
-                        'qiita-client']
+                        'requests', 'mock', 'pandas', 'six', 'jira']
       )
