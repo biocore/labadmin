7d4273f516d95c0e13be1dec435e43b1a7d5c42f
Antonio Gonzalez <antgonza@gmail.com>
2017-04-15 13:56:33 -0600

    addressing @josenavas comments

--- knimin/lib/tests/test_parse.py
+++ knimin/lib/tests/test_parse.py
@@ -97,62 +97,65 @@ class TestParse(TestCase):
             "u'Status']")
 
 
-QPCR_OBJECT_ERROR1 = """
-Include	Color	Pos	Name	CpError	Concentration	Standard	Status
-TRUE	255	A1	Sample 1	10.73		0
+QPCR_OBJECT_ERROR1 = """Experiment: Knight_kapa_qpcr  Selected Filter: \
+SYBR Green I / HRM Dye (465-510),,,,,,,
+Include,Color,Pos,Name,CpError,Concentration,Standard,Status
+True,255,A1,Sample 1,10.73,,0,
 """
 
-QPCR_OBJECT_ERROR2 = """
-Include	Color	PosError	Name	Cp	Concentration	Standard	Status
-TRUE	255	A1	Sample 1	10.73		0
+QPCR_OBJECT_ERROR2 = """Experiment: Knight_kapa_qpcr  Selected Filter: \
+SYBR Green I / HRM Dye (465-510),,,,,,,
+Include,Color,PosError,Name,Cp,Concentration,Standard,Status
+True,255,A1,Sample 1,10.73,,0,
 """
 
-QPCR_OBJECT = """
-Include	Color	Pos	Name	Cp	Concentration	Standard	Status
-TRUE	255	A1	Sample 1	10.73		0
-TRUE	255	A2	Sample 2	7.3		0
-TRUE	255	A3	Sample 3	6.77		0
-TRUE	255	A4	Sample 4	6.66		0
-TRUE	255	A5	Sample 5	7.44		0
-TRUE	255	A6	Sample 6	12.1		0
-TRUE	255	A7	Sample 7	6.63		0
-TRUE	255	A8	Sample 8	6.53		0
-TRUE	255	A9	Sample 9	9.32		0
-TRUE	255	A10	Sample 10	6.34		0
-TRUE	255	A11	Sample 11	6.05		0
-TRUE	255	A12	Sample 12	7.77		0
-TRUE	255	A13	Sample 13	11.47		0
-TRUE	255	A14	Sample 14	15.52		0
-TRUE	255	A15	Sample 15	12.6		0
-TRUE	255	A16	Sample 16	11.18		0
-TRUE	255	A17	Sample 17	21.37		0
-TRUE	255	A18	Sample 18	15.07		0
-TRUE	255	A19	Sample 19	11.45		0
-TRUE	255	A20	Sample 20	18.32		0
-TRUE	255	A21	Sample 21	12.68		0
-TRUE	255	A22	Sample 22	11.52		0
-TRUE	255	A23	Sample 23	11.34		0
-TRUE	255	A24	Sample 24	24.32		0
-TRUE	255	B1	Sample 25	6.61		0
-TRUE	255	B2	Sample 26	6.27		0
-TRUE	255	B3	Sample 27	12.11		0
-TRUE	255	B4	Sample 28	7.15		0
-TRUE	255	B5	Sample 29	6.21		0
-TRUE	255	B6	Sample 30	7.31		0
-TRUE	255	B7	Sample 31	6.87		0
-TRUE	255	B8	Sample 32	5.98		0
-TRUE	255	B9	Sample 33	6.43		0
-FALSE	255	B10	Sample 34	6.59		0
-TRUE	255	B11	Sample 35	6.5		0
-TRUE	255	B12	Sample 36	8.07		0
-TRUE	255	B13	Sample 37	7.23		0
-TRUE	255	B14	Sample 38	7.82		0
-TRUE	255	B15	Sample 39	10.09		0
-TRUE	255	B16	Sample 40	15.68		0
-TRUE	65280	B17	Sample 41			0
-TRUE	65280	B18	Sample 42			0
-TRUE	255	B19	Sample 43	14.78		0
-TRUE	255	B20	Sample 44	11.54		0
+QPCR_OBJECT = """Experiment: Knight_kapa_qpcr  Selected Filter: \
+SYBR Green I / HRM Dye (465-510),,,,,,,
+Include,Color,Pos,Name,Cp,Concentration,Standard,Status
+True,255,A1,Sample 1,10.73,,0,
+True,255,A2,Sample 2,7.3,,0,
+True,255,A3,Sample 3,6.77,,0,
+True,255,A4,Sample 4,6.66,,0,
+True,255,A5,Sample 5,7.44,,0,
+True,255,A6,Sample 6,12.1,,0,
+True,255,A7,Sample 7,6.63,,0,
+True,255,A8,Sample 8,6.53,,0,
+True,255,A9,Sample 9,9.32,,0,
+True,255,A10,Sample 10,6.34,,0,
+True,255,A11,Sample 11,6.05,,0,
+True,255,A12,Sample 12,7.77,,0,
+True,255,A13,Sample 13,11.47,,0,
+True,255,A14,Sample 14,15.52,,0,
+True,255,A15,Sample 15,12.6,,0,
+True,255,A16,Sample 16,11.18,,0,
+True,255,A17,Sample 17,21.37,,0,
+True,255,A18,Sample 18,15.07,,0,
+True,255,A19,Sample 19,11.45,,0,
+True,255,A20,Sample 20,18.32,,0,
+True,255,A21,Sample 21,12.68,,0,
+True,255,A22,Sample 22,11.52,,0,
+True,255,A23,Sample 23,11.34,,0,
+True,255,A24,Sample 24,24.32,,0,
+True,255,B1,Sample 25,6.61,,0,
+True,255,B2,Sample 26,6.27,,0,
+True,255,B3,Sample 27,12.11,,0,
+True,255,B4,Sample 28,7.15,,0,
+True,255,B5,Sample 29,6.21,,0,
+True,255,B6,Sample 30,7.31,,0,
+True,255,B7,Sample 31,6.87,,0,
+True,255,B8,Sample 32,5.98,,0,
+True,255,B9,Sample 33,6.43,,0,
+True,255,B10,Sample 34,6.59,,0,
+True,255,B11,Sample 35,6.5,,0,
+True,255,B12,Sample 36,8.07,,0,
+True,255,B13,Sample 37,7.23,,0,
+True,255,B14,Sample 38,7.82,,0,
+True,255,B15,Sample 39,10.09,,0,
+True,255,B16,Sample 40,15.68,,0,
+True,65280,B17,Sample 41,,,0,
+True,65280,B18,Sample 42,,,0,
+True,255,B19,Sample 43,14.78,,0,
+True,255,B20,Sample 44,11.54,,0,
 """
 
 PLATE_READER_EXAMPLE = """Curve0.5\tY=A*X+B\t1.15E+003\t99.8\t0.773\t?????\n
